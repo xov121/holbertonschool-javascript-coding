a57cc536a8f4238574884a27ff09aee6a8462e2c
xov121 <x.o.v121@gmail.com>
2024-05-27 11:52:47 -0400

    Task 5

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const url = require('url');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  if (parsedUrl.pathname === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (parsedUrl.pathname === '/students') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2])
+      .then(() => {
+        res.end();
+      })
+      .catch((error) => {
+        res.statusCode = 500;
+        res.end(error.message);
+      });
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
+
+const PORT = 1245;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
+
+module.exports = app;
