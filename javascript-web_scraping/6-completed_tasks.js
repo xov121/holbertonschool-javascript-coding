395411b0ed4dd35c482b26107948f58bf582b49c
xov121 <x.o.v121@gmail.com>
2024-05-27 08:26:10 -0400

    Task 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const todos = JSON.parse(body);
+    const completedTasks = {};
+
+    todos.forEach(todo => {
+      if (todo.completed) {
+        if (!completedTasks[todo.userId]) {
+          completedTasks[todo.userId] = 0;
+        }
+        completedTasks[todo.userId] += 1;
+      }
+    });
+
+    console.log(completedTasks);
+  }
+});
