4904d241bc1da1370ba7ebba53daecd1d384fb49
xov121 <x.o.v121@gmail.com>
2024-05-27 11:37:40 -0400

    Task 2

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').filter(line => line.trim() !== '');
+
+    const students = lines.slice(1).map(line => line.split(','));
+
+    const totalStudents = students.length;
+    console.log(`Number of students: ${totalStudents}`);
+
+    const fields = {};
+    students.forEach(student => {
+      const field = student[3];
+      const firstName = student[0];
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstName);
+    });
+
+    for (const [field, names] of Object.entries(fields)) {
+      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
